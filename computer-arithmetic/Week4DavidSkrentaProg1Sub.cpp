4f0d7bc0a579ac5382c57362282d0316527769c9
dskrenta <dskrenta@gmail.com>
2017-02-14 12:00:29 -0800

    changes

--- computer-arithmetic/Week4DavidSkrentaProg1Sub.cpp
+++ computer-arithmetic/Week4DavidSkrentaProg1Sub.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main () {
+  /*
   // 170
   int num1 [10] = {0, 0, 1, 0, 1, 0, 1, 0, 1, 0};
-  // 91
-  int num2 [10] = {0, 0, 0, 1, 0, 1, 1, 0, 1, 1};
+  // 91 = 00 0101 1011
+  int num2 [10] = {1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
   int carry [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   int result [10];
 
@@ -22,4 +23,23 @@ int main () {
   for (int i = 0; i < 10; i++) {
     cout << result[i] << endl;
   }
+  */
+
+  // 170
+  int num1 [10] = {0, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+  // 91 = 00 0101 1011
+  // -91 = 10100101
+  int num2 [10] = {0, 0, 0, 1, 0, 1, 1, 0, 1, 0};
+  int carry;
+  int sum[10];
+
+  for (int i = 0; i < 10; i++) {
+    sum[i] = ((num1[i] ^ num2[i]) ^ carry);
+    carry = ((num1[i] & num2[i]) | (num1[i] & carry)) | (num2[i] & carry);
+  }
+
+  for (int i = 0; i < 10; i++) {
+    cout << sum[i];
+  }
+  cout << endl;
 }
