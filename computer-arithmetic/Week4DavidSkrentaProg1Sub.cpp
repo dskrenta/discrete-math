ee7305f1c7ccc0689c068ff859489054cd6c05d4
dskrenta <dskrenta@gmail.com>
2017-02-15 13:25:55 -0800

    syncing changes

--- computer-arithmetic/Week4DavidSkrentaProg1Sub.cpp
+++ computer-arithmetic/Week4DavidSkrentaProg1Sub.cpp
@@ -2,29 +2,6 @@
 using namespace std;
 
 int main () {
-  /*
-  // 170
-  int num1 [10] = {0, 0, 1, 0, 1, 0, 1, 0, 1, 0};
-  // 91 = 00 0101 1011
-  int num2 [10] = {1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
-  int carry [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-  int result [10];
-
-  for (int i = 10; i > 0; i--) {
-    int num = num1[i] + num2[i] + carry[i];
-    if (num > 1) {
-      carry[i - 1] = 1;
-      result[i] = 0;
-    } else {
-      result[i] = num;
-    }
-  }
-
-  for (int i = 0; i < 10; i++) {
-    cout << result[i] << endl;
-  }
-  */
-
   // 170
   int num1 [10] = {0, 0, 1, 0, 1, 0, 1, 0, 1, 0};
   // 91 = 00 0101 1011
--- computer-arithmetic/Week4DavidSkrentaProg2Div.cpp
+++ computer-arithmetic/Week4DavidSkrentaProg2Div.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-  // int num1[10] = {0, 0, 0, 0, 0, 0, 1, 0, 1, 0};
-  // int divisor[10] = {0, 0, 0, 0, 0, 0, 1, 0, 1, 0};
+void divideBy10();
+void divideBy100();
 
-  int num = 100;
-  int divisor = 10;
-  int result = 0;
-  int temp;
+int main() {
+  divideBy10();
+  divideBy100();
+}
 
-  while (temp < num) {
-    temp += divisor;
-    result++;
+void divideBy10() {
+  int num1[10] = {0, 0, 0, 1, 1, 0, 0, 1, 0, 0};
+  int divisor[10] = {0, 0, 0, 0, 0, 0, 1, 0, 1, 0};
+  int num2[10] = {0, 0, 0, 0, 0, 0, 0, 1, 1, 1};
+  int sum[10];
+  int carry;
+
+  for(int i = 0; i < 10; i++) {
+    sum[i] = ((num1[i] ^ num2[i]) ^ carry);
+    carry = ((num1[i] & num2[i]) | (num1[i] & carry)) | (num2[i] & carry);
   }
 
-  cout << result << endl;
+  for(int i = 0; i < 10; i++) {
+    cout << sum[i];
+  }
+  cout << endl;
+}
+
+void divideBy100() {
+  int num1[10] = {0, 0, 0, 1, 1, 0, 0, 1, 0, 0};
+  int divisor[10] = {1, 1, 1, 1, 1, 0, 1, 0, 0, 0};
+  int num2[10] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1};
+  int sum[10];
+  int carry;
+
+  for(int i = 0; i < 10; i++) {
+    sum[i] = ((num1[i] ^ num2[i]) ^ carry);
+    carry = ((num1[i] & num2[i]) | (num1[i] & carry)) | (num2[i] & carry);
+  }
+
+  for(int i = 0; i < 10; i++) {
+    cout << sum[i];
+  }
+  cout << endl;
 }
